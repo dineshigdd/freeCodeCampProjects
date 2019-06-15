e1d89a697be71ca7263709c7cfbb266da45d8800
Dinesh Wickramasinghe <dineshigdd@gmail.com>
2019-06-15 02:20:10 -0700

    first commit

---
+++ ReactMarkdownPreviewer/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Editor from './Editor';
+
+class App extends React.Component {
+      render(){
+          return(
+              <Editor />
+          );
+      };
+
+
+}
+
+export default App;
---
+++ ReactMarkdownPreviewer/src/components/Editor.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Editor extends React.Component{
+   render(){
+     return(
+       <div>This is the Editor</div>
+     )
+   }
+}
+
+
+export default Editor;
