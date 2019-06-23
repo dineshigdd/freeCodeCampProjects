b061abcbdb5d976c3a8f68efc10c2ac097533310
dineshigdd <dineshigdd@gmail.com>
2019-06-23 11:40:20 -0700

    second commit

--- drum_machine/src/index.js
+++ drum_machine/src/index.js
@@ -2,20 +2,43 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 class DrumMachine extends React.Component{
+  state = { selectedDrum : null };
+
+  componentWillUpdate(prevProps, prevState){
+    if(this.state.selectedDrum !== prevState.selectedDrum) {
+      let track;
+
+        switch (this.state.selectedDrum) {
+            case 'Q':
+                track = 'Q'
+              break;
+            case 'W':
+                track = 'W'
+              break;
+            default:
+              break;
+        }
+
+        if( track ){
+           this.player.src = track;
+           this.player.play();
+        }
+    }
+  }
 
     render(){
       return(
         <div>
           <div id='drum-machine'>
               <ul>
-                <li className='drum-pad' id='q-trig'>Q
-                    <audio controls className='clip' id='Q'>
-                      <source src="demo.mp3" className='clip' id='Q'></source>
+                <li className='drum-pad' id='q-trig' onClick = { this.setState({ selectedDrum:'Q'})}>Q
+                      <audio  ref={ref => this.player = ref } controls>
+                      <source src='dog.mp3'  type='audio/mpeg' className='clip' id='Q'></source>
                     </audio>
                 </li>
-                <li className='drum-pad' id='w-trig'>W
-                    <audio controls>
-                      <source src="demo.mp3" className='clip' id='W'></source>
+                <li className='drum-pad' id='w-trig' onClick = { this.setState({ selectedDrum:'Q'})}>W
+                    <audio ref={ref => this.player = ref }  controls>
+                      <source src="dog.mp3" type='audio/mpeg' className='clip' id='W'></source>
                     </audio>
                 </li>
                 <li className='drum-pad' id='e-trig'>E
