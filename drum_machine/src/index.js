da8b8e314bb5f7988943a3961522198157766282
dinesh <dineshigdd@gmail.com>
2019-06-26 00:11:02 -0700

    first sound

--- drum_machine/src/index.js
+++ drum_machine/src/index.js
@@ -4,20 +4,23 @@ import ReactDom from 'react-dom';
 class DrumMachine extends React.Component{
   constructor(props){
     super(props);
-    this.myref = React.createRef();
+    this.test= this.test.bind(this);
   }
   state = { selectedDrum : null };
 
+  test(){
+    this.Q.play();
+    //this.props.setState({selectedDrum:"Q"});
+  }
 
     render(){
       return(
         <div>
           <div id='drum-machine'>
               <ul>
-                <li className='drum-pad' id='q-trig'>Q
-                      <audio myref={ this.myref }>
-                          <source src="https://s3.amazonaws.com/freecodecamp/simonSound1.mp3" type="audio/mpeg" >
-                          </source>
+                <li className='drum-pad' id='q-trig' onClick={ this.test }>Q
+                      <audio  ref={ Q => (this.Q = Q)} src="https://s3.amazonaws.com/freecodecamp/simonSound1.mp3" type="audio/mpeg">
+
                       </audio>
                 </li>
               </ul>
