df71459ff1dcd5e0b8258e972d805094731e81b7
dineshigdd <dineshigdd@gmail.com>
2019-06-25 18:13:19 -0700

    third commit

--- drum_machine/src/index.js
+++ drum_machine/src/index.js
@@ -2,79 +2,23 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 class DrumMachine extends React.Component{
+  constructor(props){
+    super(props);
+    this.myref = React.createRef();
+  }
   state = { selectedDrum : null };
 
-  componentWillUpdate(prevProps, prevState){
-    if(this.state.selectedDrum !== prevState.selectedDrum) {
-      let track;
-
-        switch (this.state.selectedDrum) {
-            case 'Q':
-                track = 'Q'
-              break;
-            case 'W':
-                track = 'W'
-              break;
-            default:
-              break;
-        }
-
-        if( track ){
-           this.player.src = track;
-           this.player.play();
-        }
-    }
-  }
 
     render(){
       return(
         <div>
           <div id='drum-machine'>
               <ul>
-                <li className='drum-pad' id='q-trig' onClick = { this.setState({ selectedDrum:'Q'})}>Q
-                      <audio  ref={ref => this.player = ref } controls>
-                      <source src='dog.mp3'  type='audio/mpeg' className='clip' id='Q'></source>
-                    </audio>
-                </li>
-                <li className='drum-pad' id='w-trig' onClick = { this.setState({ selectedDrum:'Q'})}>W
-                    <audio ref={ref => this.player = ref }  controls>
-                      <source src="dog.mp3" type='audio/mpeg' className='clip' id='W'></source>
-                    </audio>
-                </li>
-                <li className='drum-pad' id='e-trig'>E
-                    <audio controls>
-                      <source src="demo.mp3" className='clip' id='E'></source>
-                    </audio>
-                </li>
-                <li className='drum-pad' id='a-trig'>A
-                    <audio controls>
-                      <source src="demo.mp3" className='clip' id='A'></source>
-                    </audio>
-                </li>
-                <li className='drum-pad' id='s-trig'>S
-                    <audio controls>
-                      <source src="demo.mp3" className='clip' id='S'></source>
-                    </audio>
-                </li>
-                <li className='drum-pad' id='d-trig'>D
-                    <audio controls>
-                      <source src="demo.mp3" className='clip' id='D'></source>
-                    </audio>
-                </li>
-                <li className='drum-pad' id='z-trig'>Z
-                    <audio controls>
-                      <source src="demo.mp3" className='clip' id='Z'></source>
-                    </audio>
-                </li>
-                <li className='drum-pad' id='x-trig'>X
-                    <audio controls>
-                      <source src="demo.mp3" className='clip' id='X'></source>
-                    </audio>
-                </li>
-                <li className='drum-pad' id='c-trig'>C
-                    <audio controls>
-                      <source src="demo.mp3" className='clip' id='C'></source>
-                    </audio>
+                <li className='drum-pad' id='q-trig'>Q
+                      <audio myref={ this.myref }>
+                          <source src="https://s3.amazonaws.com/freecodecamp/simonSound1.mp3" type="audio/mpeg" >
+                          </source>
+                      </audio>
                 </li>
               </ul>
               </div>
@@ -82,6 +26,6 @@ class DrumMachine extends React.Component{
         </div>
       );
     };
-}
+  }
 
 ReactDom.render(<DrumMachine />, document.querySelector('#root'));
