60b4fa3e762b0881fa00fa094f552ab8ead98ac6
dinesh <dineshigdd@gmail.com>
2019-07-14 13:03:12 -0700

    ui for clock

--- promodoro-clock/src/index.js
+++ promodoro-clock/src/index.js
@@ -9,20 +9,32 @@ class PomodoroClock extends React.Component{
 render(){
   return(
     <div id="timer-container">
-        <div id='top-label'>
-          <div id="break-label">Break Length</div>
-          <div id="session-label">Session Length</div>
-        </div>
-          <div id="break-length" onload="5"></div>
-          <div id="session-length"></div>
-          <div id="timer-label">Session</div>
-          <div id="time-left"></div>
-          <button id="break-decrement">Break Increase</button>
-          <button id="session-decrement">Session Decrease</button>
-          <button id="break-increment">Break Increase</button>
-          <button id="session-increment">Session Increase</button>
-          <button id="start_stop">Stop</button>
-          <button id="reset">Reset</button>
+        <ul id="display">
+            <li><div id="timer-label">Session</div>
+            <div id="time-left">25:00</div></li>
+        </ul>
+        <ul id="control">
+            <button id="start_stop"><i className="play icon"></i><i className="stop icon"></i></button>
+            <button id="reset"><i className="sync icon"></i></button>
+        </ul>
+        <ul id='settings'>
+          <li>
+              <div id="break-label">Break Length</div>
+                <div className='settings-btn-move'>
+                    <button id="break-decrement"><i className="angle down icon"></i></button>
+                    <div id="break-length" className="div-padding">5</div>
+                    <button id="break-increment"><i className="angle up icon"></i></button>
+                </div>
+          </li>
+          <li>
+              <div id="session-label">Session Length</div>
+              <div className='settings-btn-move' id="settings-session-btn-move">
+                  <button id="session-decrement"><i className="angle down icon"></i></button>
+                  <div id="session-length"  className="div-padding">25</div>
+                  <button id="session-increment"><i className="angle up icon"></i></button>
+              </div>
+          </li>
+      </ul>
     </div>
     );
   };
