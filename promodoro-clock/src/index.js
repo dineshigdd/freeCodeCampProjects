649c5df6716eabc9e3ada949eea7c6eba208c811
dinesh <dineshigdd@gmail.com>
2019-07-17 10:58:43 -0700

    fix inccrease, decrease and display

--- promodoro-clock/src/index.js
+++ promodoro-clock/src/index.js
@@ -1,15 +1,19 @@
 //https://reactjs.org/docs/state-and-lifecycle.html#adding-lifecycle-methods-to-a-class
+// https://stackoverflow.com/questions/8126466/how-do-i-reset-the-setinterval-timer
 import React from 'react';
 import ReactDom from 'react-dom';
 import '../src/styles.css';
 
-var isMytimer = 'off';
+var isMytimerRuning = 'off';
 let mytimer = '';
+let isReset = false;
+
 class PomodoroClock extends React.Component{
   constructor(props){
     super(props);
 
     mytimer = setInterval( () => this.timer(),1000);
+
     this.state = {
       mytimer:'',
       breaklength:5,
@@ -18,41 +22,75 @@ class PomodoroClock extends React.Component{
     };
 
   this.reset = this.reset.bind(this);
-
+  this.addLeadingZerosTOSeconds.bind(this);
+  this.addLeadingZerosTOSession.bind(this);
   }
 
+addLeadingZerosTOSeconds( stateValue ){
+  let num = (Number(stateValue) + 59)%60 ;
 
+  if( num < 10 ){
+     num = '0' + num;
+  }
+  return num;
+}
+
+addLeadingZerosTOSession( stateValue ){
+  let session = Number(stateValue);
+  if( session < 10 ){
+    session = '0' + session;
+  }
+
+  return session;
+}
+
+timerController(){
+  if( isReset ){
+    mytimer = setInterval( () => this.timer(),1000);
+  }else{
+    if( isMytimerRuning === 'off'){
+      isMytimerRuning = 'on';
+    }else{
+      isMytimerRuning = 'off';
+    }
+  }
+}
 
 timer(){
- if( isMytimer === 'on'){
-   while( !isMytimer ){
-   console.log("isMytimer:" +isMytimer)
-   this.setState( { seconds: (this.state.seconds + 59)%60 });
-             if( this.state.seconds === 0 ){
-                   this.setState( { seconds: '00'});
-                   this.setState( { sessionlength: this.state.sessionlength - 1});
-             }
+       if( isMytimerRuning === 'on'){
+           console.log("isMytimer:" +isMytimerRuning)
 
-             // if( isMytimer === 'off'){
-             //   clearInterval(this.mytimer);
-             // }
-  }
+           // let second = (Number(this.state.seconds) + 59)%60 ;
+           //
+           // if( second < 10 ){
+           //    second = '0' + second;
+           // }
+           this.setState( { seconds:  this.addLeadingZerosTOSeconds(this.state.seconds) });
+                   if( this.state.seconds === "00" ){
+                         // let session = Number(this.state.sessionlength) - 1;
+                         // if( session < 10 ){
+                         //   session = '0' + session;
+                         // }
+                         this.setState( { sessionlength:this.addLeadingZerosTOSession(this.state.sessionlength - 1) });
+                   }
+        }else{
+            clearInterval(this.mytimer);
+            console.log("isMytimer:" +isMytimerRuning)
+        }
 
-  //   isMytimer = "off";
-  // }else{
-  //   console.log("isMytimer:" +isMytimer)
-  //   clearInterval(this.mytimer);
-  //   isMytimer = "on";
-  //   }
-  }
+}
 
 
 reset(){
+  //isMytimer = 'off';
+  isReset = true;
+  clearInterval(mytimer);
   this.setState({ sessionlength: 25 })
   this.setState({ breaklength: 5 })
+  this.setState({ seconds: '00' })
 }
 
-breaklengthDrecrease(type ){
+decrease(type ){
 
 if(  type === 'breaklength' ){
     if(  this.state.breaklength < 2 ){
@@ -61,27 +99,40 @@ if(  type === 'breaklength' ){
             this.setState({ breaklength : this.state.breaklength  - 1   });
     }
   }else{
-    if(  this.state.sessionlength < 2 ){
-            this.setState({ sessionlength : 1 });
-      }else{
-            this.setState({ sessionlength : this.state.sessionlength  - 1   });
-    }
+        let session = this.state.sessionlength  - 1  ;
+              if( this.state.sessionlength  <= 10 ){
+                session = this.addLeadingZerosTOSession( session );
+
+                if( Number(session) < 2 ){
+                  session = '01';
+                }
+              }
+
+              this.setState({ sessionlength : session });
   }
 }
 
- breaklengthIncrease(type){
+ increase(type){
    if(  type === 'breaklength' ){
        if(  this.state.breaklength < 2 ){
                this.setState({ breaklength : 1 });
-         }else{
+         }else if( this.state.breaklength < 60){
                this.setState({ breaklength : this.state.breaklength  + 1   });
        }
      }else{
-       if(  this.state.sessionlength < 2 ){
-               this.setState({ sessionlength : 1 });
-         }else{
-               this.setState({ sessionlength : this.state.sessionlength  + 1   });
-       }
+       let session = Number(this.state.sessionlength) + 1;
+             if( session > 60 ){
+               session = 60;
+             }else
+             if( this.state.sessionlength  <= 10 ){
+               session = this.addLeadingZerosTOSession( session );
+
+               if( Number(session) < 2 ){
+                 session = '01';
+               }
+             }
+
+             this.setState({ sessionlength : session });
      }
  }
 
@@ -90,27 +141,27 @@ render(){
     <div id="timer-container">
         <ul id="display">
             <li><div id="timer-label">Session</div>
-            <div id="time-left">{ this.state.sessionlength}: { this.state.seconds } </div></li>
+            <div id="time-left">{ this.state.sessionlength}:{ this.state.seconds }</div></li>
         </ul>
         <ul id="control">
-            <button id="start_stop" onClick={ this.timer.bind(this) }><i className="play icon"></i><i className="stop icon"></i></button>
+            <button id="start_stop" onClick={ this.timerController.bind(this) }><i className="play icon"></i><i className="stop icon"></i></button>
             <button id="reset" onClick={ this.reset }><i className="sync icon"></i></button>
         </ul>
         <ul id='settings'>
           <li>
               <div id="break-label">Break Length</div>
                 <div className='settings-btn-move'>
-                    <button id="break-decrement" onClick={ this.breaklengthDrecrease.bind(this,'breaklength') }><i className="angle down icon"></i></button>
+                    <button id="break-decrement" onClick={ this.decrease.bind(this,'breaklength') }><i className="angle down icon"></i></button>
                     <div id="break-length" className="div-padding">{ this.state.breaklength }</div>
-                    <button id="break-increment" onClick={ this.breaklengthIncrease.bind(this,'breaklength')}><i className="angle up icon"></i></button>
+                    <button id="break-increment" onClick={ this.increase.bind(this,'breaklength')}><i className="angle up icon"></i></button>
                 </div>
           </li>
           <li>
               <div id="session-label">Session Length</div>
               <div className='settings-btn-move' id="settings-session-btn-move">
-                  <button id="session-decrement" onClick={ this.breaklengthDrecrease.bind(this)}><i className="angle down icon"></i></button>
+                  <button id="session-decrement" onClick={ this.decrease.bind(this)}><i className="angle down icon"></i></button>
                   <div id="session-length"  className="div-padding">{ this.state.sessionlength }</div>
-                  <button id="session-increment" onClick={ this.breaklengthIncrease.bind(this)}><i className="angle up icon"></i></button>
+                  <button id="session-increment" onClick={ this.increase.bind(this)}><i className="angle up icon"></i></button>
               </div>
           </li>
       </ul>
