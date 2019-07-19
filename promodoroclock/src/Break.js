73dac307a30d58ece6cdd0b28295cf67e13ee569
dinesh <dineshigdd@gmail.com>
2019-07-18 20:31:33 -0700

    first commit

---
+++ promodoroclock/src/Break.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class Break extends React.Component {
+
+  render(){
+    return(
+          <div>
+            this is break
+
+          </div>
+
+    );
+  }
+}
+
+export default Break;
